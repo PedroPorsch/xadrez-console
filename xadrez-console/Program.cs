aa27010fa4914a98e6d6d5954615a7eedd48f30e
Pedro Porsch Spainol <pedroporschspaniol@gmail.com>
2023-12-20 10:36:02 -0300

    Classe posição foi criada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using Tabuleiro;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Posicao p = new Posicao(1, 0);
+            Console.WriteLine("Posição "  + p);
+
+            Console.ReadLine();
+        }
+    }
+}
